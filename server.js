8544fecf7be88353c2bb68de6ea3b170c45afcb7
Thamila92 <achatthamila@gmail.com>
2025-02-10 19:14:01 +0100

    add data json server

--- server.js
+++ server.js
@@ -2,44 +2,71 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
 const PORT = 8080;
+const dataFilePath = path.join(__dirname, 'data.json');
 
- app.use(bodyParser.json());
+app.use(bodyParser.json());
 
- const swaggerDocument = YAML.load('./swagger-steup.yaml');
+const swaggerDocument = YAML.load('./Swagger.yml');
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+ function readData() {
+    try {
+        const jsonData = fs.readFileSync(dataFilePath);
+        return JSON.parse(jsonData);
+    } catch (err) {
+        return { cards: [] };   
+    }
+}
+
+ function saveData(data) {
+    const dataString = JSON.stringify(data, null, 2);
+    fs.writeFileSync(dataFilePath, dataString);
+}
+
  app.get('/cards', (req, res) => {
-     res.status(200).json([
-        { id: '1', question: 'What is Node.js?', answer: 'A JavaScript runtime environment.', category: 'First' }
-    ]);
+    const data = readData();
+    res.status(200).json(data.cards);
 });
 
  app.post('/cards', (req, res) => {
-     const newCard = { id: '2', ...req.body };
-    console.log(newCard);   
+    const newCard = { id: Date.now().toString(), ...req.body };
+    const data = readData();
+    data.cards.push(newCard);
+    saveData(data);
     res.status(201).json({ message: 'Card created', card: newCard });
 });
 
  app.patch('/cards/:cardId/answer', (req, res) => {
-     console.log(req.params.cardId, req.body);
-    res.status(204).send();  
+    const { cardId } = req.params;
+    const { isValid } = req.body;
+    const data = readData();
+    const card = data.cards.find(card => card.id === cardId);
+    if (card) {
+        card.answeredCorrectly = isValid;
+        saveData(data);
+        res.status(204).send();
+    } else {
+        res.status(404).send('Card not found');
+    }
 });
 
-app.get('/cards/quizz', (req, res) => {
-    const { date } = req.query; 
-    
-    res.status(200).json([
-        { id: '1', question: 'What is Express?', answer: 'A web framework for Node.js', category: 'First' }
-    ]);
+ app.get('/cards/quizz', (req, res) => {
+    const { date } = req.query;
+    const data = readData();
+    //logique pour filter les crtes  (ps encore faite )
+     res.status(200).json(data.cards);
 });
- app.get('/', (req, res) => {
+
+app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
- app.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
     console.log(`Swagger API documentation available at http://localhost:${PORT}/api-docs`);
 });
