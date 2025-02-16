a029f266f77dda0aad72b300f7e1187a775525d0
MAZOUNI Mohamed Rayan <88382756+mazounirayan@users.noreply.github.com>
2025-02-16 19:19:38 +0100

    cors

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
+import cors from 'cors'; // Import the cors package
 import { CardHandler } from './routes/cardRoutes.js';
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080; 
+
+app.use(cors());
 
 app.use(bodyParser.json());
 
